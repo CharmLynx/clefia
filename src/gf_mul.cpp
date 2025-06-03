c1b8429e85155ad0876feaca73367202181755d4
CharmLynx <maryna.maliutina@gmail.com>
2025-06-04 00:52:26 +0300

    fix mul

--- src/gf_mul.cpp
+++ src/gf_mul.cpp
@@ -7,15 +7,15 @@ using namespace std;
 uint8_t gf_mul(uint8_t a, uint8_t b) {
     uint16_t res = 0;
     uint16_t aa = a;
-    for (int i = 0; i < 8; ++i) {
-        if (b & 1)
-            res ^= aa;
+    for (int i=0; i<8; ++i) {
+        if (b&1)
+            res^=aa;
 
-        aa <<= 1;
-        if (aa & 0x100)
-            aa ^= 0x11D;
+        aa<<=1;
+        if (aa&0x100)
+            aa^=0x11D;
 
-        b >>= 1;
+        b>>=1;
     }
     return res & 0xFF;
 }
