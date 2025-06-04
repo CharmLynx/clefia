f5014df1550d2fd23a549fad283c202fabed215f
CharmLynx <maryna.maliutina@gmail.com>
2025-06-04 21:05:24 +0300

    fix utils

--- include/utils.hpp
+++ include/utils.hpp
@@ -3,7 +3,5 @@
 #include <string>
 #include <cstdint>
 
-
-void hex_to_bytes(const std::string& hex_string, uint64_t digits[], int& count);
-
-void bytes_to_hex(uint64_t A[], std::string& a, int count);
+void hex_to_bytes(const std::string& hex_string, uint8_t digits[], int& count);
+void bytes_to_hex(uint8_t A[], std::string& a, int count);
--- src/utils.cpp
+++ src/utils.cpp
@@ -2,35 +2,23 @@
 #include <sstream>     
 #include <iomanip>    
 #include <cstdint>   
-#include <iostream>
-#include <cmath>
+
+#include "utils.hpp"
+#include "clefia.hpp"
+
 
 using namespace std;
 
-void hex_to_bytes(const string& hex_string, uint64_t digits[], int& count){
-
-    int hex_length = hex_string.length();
-    string text = hex_string;
-    if(hex_length%8!=0){
-        int padding=8-(hex_length%8);
-        text=string(padding, '0')+text;
-    }
-    int text_length = text.length();
-    string part(8,'0');
-    int n=0;
-    while(text_length>0){
-        for(int i=0;i<8;i++){
-                part.at(i)=text.at(i+n);
-        }
-        n+=8;
-        text_length=text_length-8;
-        uint32_t value = stoul(part, nullptr, 16);
-        digits[count]=value;
-        count++;
+void hex_to_bytes(const string& hex_string, uint8_t output[], int& count){
+    count = 0;
+    for(size_t i = 0; i < hex_string.length(); i += 2){
+        string byte_str = hex_string.substr(i, 2);
+        output[count++] = static_cast<uint8_t>(stoi(byte_str, nullptr, 16));
     }
 }
 
-void bytes_to_hex(uint64_t A[], string& a, int count){
+
+void bytes_to_hex(uint8_t A[], string& a, int count){
     for(int i =0;i<count;i++){
         stringstream ss;
         ss << setfill('0') << setw(8) << hex << A[i];
